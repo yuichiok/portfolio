ab1b4b6237e488d11045633a6ada51552dc83965
yuichiok <yuichi.okugawa@cern.ch>
2024-02-23 16:30:43 +0900

    Update resume fileUrl

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -30,7 +30,8 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      // 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://yuichiok.github.io/folder/CV_okugawa.pdf',
   },
   skills: [
     'C++',
